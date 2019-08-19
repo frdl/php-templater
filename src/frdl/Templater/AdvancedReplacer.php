29e9cbb4dc39aa05c81e6d6bfe9a5ca6271451a1
Till Wehowski <github@webfan.de>
2019-08-19 08:56:56 +0200

    Create AdvancedReplacer.php

---
+++ src/frdl/Templater/AdvancedReplacer.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace frdl\Templater;
+
+use frdl\Context as Context;
+
+class AdvancedReplacer extends Replacer
+{
+	
+	protected $filters;
+	protected $templating;
+	
+	public function __construct(){
+	   $this->filters = [
+	      'uppercase' => 'strtoupper',
+	      'lowercase' => 'strtolower',
+	      'ucfirst' => 'ucfirst',
+	      'json' => 'json_encode',
+	      'json_encode' => 'json_encode',
+	      'json_decode' => 'json_decode'
+	   
+	   ];	
+		
+		$this->templating = new Templating();
+	}
+		
+	public function getTemplating(){
+		return $this->templating;
+	}
+	
+	public function getFilters(){
+		return $this->filters;
+	}
+		
+	public function setFilters(array $filters){
+		$this->filters = $filters;	
+	}
+	
+    public function replace(Context $context, string $template) : string {
+		$self = new self;
+		return $context(function($ArrayObject) use($self, $template){
+		     return $self->getTemplating()->render( $template, $ArrayObject->all(), $self->getFilters() );					
+		});
+
+	}
+	
+}
